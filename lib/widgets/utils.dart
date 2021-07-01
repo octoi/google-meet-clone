7d8263d839b04baf2a368193a4a553493bc0be89
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-01 21:48:35 +0530

    call end btn

--- lib/widgets/utils.dart
+++ lib/widgets/utils.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_meet/utils/constants.dart';
 
 class AppUtils extends StatelessWidget {
   const AppUtils({Key? key}) : super(key: key);
@@ -6,8 +7,21 @@ class AppUtils extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: EdgeInsets.all(8.0),
       child: Row(
-        children: [],
+        children: [
+          Container(
+            padding: EdgeInsets.all(15.0),
+            decoration: BoxDecoration(
+              color: appRed,
+              borderRadius: BorderRadius.circular(50.0),
+            ),
+            child: Icon(
+              Icons.call_end,
+              color: appWhite,
+            ),
+          ),
+        ],
       ),
     );
   }

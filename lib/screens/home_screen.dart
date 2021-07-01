7be836ad5c6a3b1cc6bcb518409c4e6142098084
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-01 21:27:28 +0530

    used header

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_meet/utils/constants.dart';
+import 'package:google_meet/widgets/header.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -9,9 +10,14 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: appPrimaryColor,
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [],
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              AppHeader(),
+            ],
+          ),
         ),
       ),
     );

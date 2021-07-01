bd466cf513f382d587e52076b7735f62a82a14cd
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-01 22:03:44 +0530

    data

--- lib/utils/data.dart
+++ lib/utils/data.dart
@@ -9,3 +9,11 @@ class Participant {
     required this.color,
   });
 }
+
+List<Participant> roomUsers = [
+  Participant(name: "Panda", color: Colors.blueGrey),
+  Participant(name: "Tiger", color: Colors.brown),
+  Participant(name: "Dog", color: Colors.pink),
+  Participant(name: "Fox", color: Colors.purple),
+  Participant(name: "Elephant", color: Colors.cyan),
+];

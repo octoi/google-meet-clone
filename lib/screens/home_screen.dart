da5264cd02520683aba236064e3eb1cd13f9c369
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-01 21:06:01 +0530

    basic

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:google_meet/utils/constants.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: appPrimaryColor,
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [],
+        ),
+      ),
+    );
   }
 }

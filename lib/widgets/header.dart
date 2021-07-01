0b08f567ebb8deacbb1e5e895f6a4cbcae205ef8
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-01 21:19:58 +0530

    header

---
+++ lib/widgets/header.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:google_meet/utils/constants.dart';
+
+class AppHeader extends StatelessWidget {
+  const AppHeader({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Row(
+          children: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.arrow_back),
+              color: appWhite,
+            ),
+            SizedBox(width: 10.0),
+            Text(
+              "abc-dc1-034",
+              style: TextStyle(
+                color: appWhite,
+                fontSize: 20.0,
+              ),
+            )
+          ],
+        )
+      ],
+    );
+  }
+}

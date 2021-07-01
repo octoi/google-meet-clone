5576c75dfad48a19035e3eb17ee151c180ab55c9
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-01 22:10:30 +0530

    looped through things and created cards

--- lib/widgets/body.dart
+++ lib/widgets/body.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_meet/utils/constants.dart';
+import 'package:google_meet/utils/data.dart';
 
 class AppBody extends StatelessWidget {
   const AppBody({Key? key}) : super(key: key);
@@ -6,7 +8,32 @@ class AppBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: Text("Hello World"),
+      child: GridView.count(
+        padding: EdgeInsets.all(8.0),
+        physics: BouncingScrollPhysics(),
+        crossAxisCount: 2,
+        children: roomUsers.map((user) => UserCard(user: user)).toList(),
+      ),
+    );
+  }
+}
+
+class UserCard extends StatelessWidget {
+  final Participant user;
+
+  const UserCard({Key? key, required this.user}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(5.0),
+      decoration: BoxDecoration(
+        color: appSecondaryColor,
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: Center(
+        child: Text(user.name),
+      ),
     );
   }
 }

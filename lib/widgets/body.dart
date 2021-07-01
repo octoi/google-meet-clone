e97a54d727a3ad7d577f7501e6f33638a998fbbb
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-01 21:42:44 +0530

    basic components

---
+++ lib/widgets/body.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppBody extends StatelessWidget {
+  const AppBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Text("Hello World"),
+    );
+  }
+}
---
+++ lib/widgets/utils.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppUtils extends StatelessWidget {
+  const AppUtils({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        children: [],
+      ),
+    );
+  }
+}

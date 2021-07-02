4c2f87f1276d7f26f434e87de627ac8b3f00819b
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-02 16:34:51 +0530

    extracted user card

--- lib/widgets/body.dart
+++ lib/widgets/body.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:google_meet/utils/constants.dart';
 import 'package:google_meet/utils/data.dart';
+import 'body_widgets/user_card.dart';
 
 class AppBody extends StatelessWidget {
   const AppBody({Key? key}) : super(key: key);
@@ -13,106 +13,6 @@ class AppBody extends StatelessWidget {
         physics: BouncingScrollPhysics(),
         crossAxisCount: 2,
         children: roomUsers.map((user) => UserCard(user: user)).toList(),
-      ),
-    );
-  }
-}
-
-class UserCard extends StatelessWidget {
-  final Participant user;
-
-  const UserCard({Key? key, required this.user}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(5.0),
-      decoration: BoxDecoration(
-        color: appSecondaryColor,
-        borderRadius: BorderRadius.circular(10.0),
-      ),
-      child: Stack(
-        children: [
-          Center(
-            child: CircleAvatar(
-              backgroundColor: user.color,
-              child: Center(
-                child: Text(
-                  user.name[0],
-                  style: TextStyle(fontSize: 25.0, color: appWhite),
-                ),
-              ),
-              radius: 35.0,
-            ),
-          ),
-          Positioned(
-            bottom: 15.0,
-            left: 15.0,
-            child: Text(
-              user.name,
-              style: TextStyle(
-                color: appWhite,
-                fontSize: 16.0,
-              ),
-            ),
-          ),
-          Positioned(
-            top: 10.0,
-            right: 10.0,
-            child: Container(
-              padding: EdgeInsets.all(2.0),
-              decoration: BoxDecoration(
-                color: appGrey,
-                borderRadius: BorderRadius.circular(50.0),
-              ),
-              child: Icon(
-                Icons.mic_off,
-                size: 20.0,
-                color: appWhite,
-              ),
-            ),
-          ),
-          Positioned(
-            bottom: 15.0,
-            right: 10.0,
-            child: InkWell(
-              onTap: () {
-                showModalBottomSheet(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return Container(
-                      padding: EdgeInsets.all(20.0),
-                      decoration: BoxDecoration(
-                        color: appWhite,
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(20.0),
-                          topRight: Radius.circular(20.0),
-                        ),
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            user.name,
-                            style: TextStyle(
-                              fontSize: 20.0,
-                            ),
-                          ),
-                          SizedBox(height: 10.0),
-                          Divider(),
-                        ],
-                      ),
-                    );
-                  },
-                );
-              },
-              child: Icon(
-                Icons.more_vert,
-                color: appWhite,
-              ),
-            ),
-          ),
-        ],
       ),
     );
   }

f756e3a1c32537791f7a261abc137a80920160d3
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-02 16:58:14 +0530

    cancel

--- lib/widgets/body_widgets/sheet_button.dart
+++ lib/widgets/body_widgets/sheet_button.dart
@@ -5,24 +5,31 @@ class SheetButton extends StatelessWidget {
   final IconData icon;
   final String title;
   final bool disabled;
+  final bool cancel;
 
   const SheetButton({
     Key? key,
     required this.icon,
     required this.title,
     this.disabled = false,
+    this.cancel = false,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return TextButton(
-      onPressed: disabled ? null : () {},
+      onPressed: disabled
+          ? null
+          : () {
+              if (cancel) Navigator.pop(context);
+            },
       style: TextButton.styleFrom(
         primary: appSecondaryColor,
+        padding: EdgeInsets.symmetric(vertical: 10.0),
       ),
       child: Row(
         children: [
-          Icon(icon),
+          Icon(icon, color: disabled ? appGrey : appSecondaryColor),
           SizedBox(width: 10.0),
           Text(title),
         ],

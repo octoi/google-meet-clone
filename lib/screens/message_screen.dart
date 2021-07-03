d0247d57c15ce5866e68b2c1b141828dcb5fdce7
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-03 13:57:39 +0530

    app bar

--- lib/screens/message_screen.dart
+++ lib/screens/message_screen.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:google_meet/utils/constants.dart';
 
 class MessageScreen extends StatelessWidget {
   const MessageScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: appWhite,
+      appBar: AppBar(
+        backgroundColor: appWhite,
+        elevation: 0.0,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(Icons.close),
+          color: appGrey,
+        ),
+        title: Text(
+          "In-call messages",
+          style: TextStyle(
+            color: appSecondaryColor,
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+      ),
+    );
   }
 }

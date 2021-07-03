772b3ccd7e52dbc87898974fb20f76b358af5165
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-03 13:54:54 +0530

    using util icon btn with message

--- lib/widgets/utils.dart
+++ lib/widgets/utils.dart
@@ -24,6 +24,7 @@ class AppUtils extends StatelessWidget {
                   UtilIconButton(
                     icon: Icons.question_answer_outlined,
                     title: "In call messages",
+                    isMessageButton: true,
                   ),
                   UtilIconButton(
                     icon: Icons.present_to_all_outlined,

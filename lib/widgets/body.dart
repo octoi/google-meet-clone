2dd14761d68c937ab37545bf10bdef18a97af007
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-03 12:38:59 +0530

    more

--- lib/widgets/body.dart
+++ lib/widgets/body.dart
@@ -20,11 +20,21 @@ class AppBody extends StatelessWidget {
           bottom: 10.0,
           right: 20.0,
           child: Container(
-            decoration: BoxDecoration(
-              color: appPrimaryColor.withOpacity(0.8),
-              borderRadius: BorderRadius.circular(10.0),
+            padding: EdgeInsets.symmetric(
+              vertical: 8.0,
+              horizontal: 10.0,
             ),
-            child: Text("Hello world"),
+            decoration: BoxDecoration(
+              color: appPrimaryColor.withOpacity(0.9),
+              borderRadius: BorderRadius.circular(20.0),
+            ),
+            child: Text(
+              '${roomUsers.length} More',
+              style: TextStyle(
+                color: appWhite,
+                fontSize: 15.0,
+              ),
+            ),
           ),
         )
       ]),

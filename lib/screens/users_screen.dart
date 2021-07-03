7c7adb1ec003e0aded1753464a81346c37f6d857
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-03 12:45:03 +0530

    app bar

---
+++ lib/screens/users_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:google_meet/utils/constants.dart';
+
+class UsersScreen extends StatelessWidget {
+  const UsersScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: appWhite,
+      appBar: AppBar(
+        backgroundColor: appWhite,
+        elevation: 0.0,
+        leading: IconButton(
+          onPressed: () => Navigator.pop(context),
+          icon: Icon(Icons.arrow_back),
+          color: appSecondaryColor,
+        ),
+      ),
+    );
+  }
+}

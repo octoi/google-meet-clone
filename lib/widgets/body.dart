47beb97d6bed9fd35384265032b9ee122394a72b
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-03 12:36:41 +0530

    create sample on stack

--- lib/widgets/body.dart
+++ lib/widgets/body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_meet/utils/constants.dart';
 import 'package:google_meet/utils/data.dart';
 import 'body_widgets/user_card.dart';
 
@@ -8,12 +9,25 @@ class AppBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: GridView.count(
-        padding: EdgeInsets.all(8.0),
-        physics: BouncingScrollPhysics(),
-        crossAxisCount: 2,
-        children: roomUsers.map((user) => UserCard(user: user)).toList(),
-      ),
+      child: Stack(children: [
+        GridView.count(
+          padding: EdgeInsets.all(8.0),
+          physics: BouncingScrollPhysics(),
+          crossAxisCount: 2,
+          children: roomUsers.map((user) => UserCard(user: user)).toList(),
+        ),
+        Positioned(
+          bottom: 10.0,
+          right: 20.0,
+          child: Container(
+            decoration: BoxDecoration(
+              color: appPrimaryColor.withOpacity(0.8),
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            child: Text("Hello world"),
+          ),
+        )
+      ]),
     );
   }
 }

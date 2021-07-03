2bf446399df8276666201c464da9062c11226002
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-03 13:03:54 +0530

    basic user tab

---
+++ lib/widgets/tab_screen/user_tab.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class UserTab extends StatelessWidget {
+  const UserTab({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      physics: BouncingScrollPhysics(),
+      child: Column(
+        children: [],
+      ),
+    );
+  }
+}
